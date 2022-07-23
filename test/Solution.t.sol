c2744d467970bf6b16bcfa6847cd1fc8812c05d2
Richie <devtooligan@gmail.com>
2022-07-22 17:13:18 -0700

    wip

--- test/Solution.t.sol
+++ test/Solution.t.sol
@@ -7,7 +7,7 @@ import "forge-std/Test.sol";
 interface Vault {
     function owner() external returns (uint);
     function setOwner(address) external returns (bool);
-    function deposit() payable external;
+    function deposit() external;
     function withdraw() external;
 
     event Deposit(address,uint256);
@@ -28,29 +28,30 @@ contract SolutionTest is Test {
         vm.deal(address(this), 1000 ether);
         // vault = Vault(HuffDeployer.deploy("Vault"));
         // vm.deal(address(vault), 1 ether);
-        HuffDeployer.config()
-            .with_value(1 ether)
-            .deploy{value: 1 ether}("src/Vault");
-        hackerStartingBal = hackerStartingBal;
-        vm.deal(hacker, hackerStartingBal);
+        vault = Vault(HuffDeployer.config()
+            .with_value(111 ether)
+            .deploy{value: 111 ether}("src/Vault"));
+        // hackerStartingBal = hackerStartingBal;
+        // vm.deal(hacker, hackerStartingBal);
 
     }
 
     /// @dev Ensure that you can set and get the value.
     function testHack() public {
 
-        console.log('before hack balance:', address(hacker).balance);
+        // console.log('contract balance:', address(vault).balance);
+        // console.log('before hack balance:', address(hacker).balance);
         // assertEq(address(hacker).balance, hackerStartingBal);
-        console.log(vault.owner());
-        vault.deposit();
+        vault.owner();
+        // vault.deposit();
         // vm.prank(hacker);
         // vault.deposit{value: 0.1 ether}();
         // bytes memory payload = abi.encodePacked(uint(0x6969696900000000000000000000000000000000000000000000000000000000), uint(0x0000004600000000000000000000000000000000000000000000000000000000));
         // // console.log(uint(bytes32(payload)));
         // (bool success, bytes memory returnData) = address(vault).call{value: 1 ether}(payload);
         // require(success);
-        console.log('after hack balance:', address(hacker).balance);
-        console.log("hello");
+        // console.log('after hack balance:', address(hacker).balance);
+        // console.log("hello");
     }
 }
 
