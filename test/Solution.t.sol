fed984d4174a6ecfa16c3ee6b79ab31de747a1e8
Richie <devtooligan@gmail.com>
2022-07-26 19:10:07 -0700

    refactor

--- test/Solution.t.sol
+++ test/Solution.t.sol
@@ -24,10 +24,7 @@ contract SolutionTest is Test {
         vm.startPrank(hacker);
         // Try sending 0x69696969 as first 4 bytes to trigger to get into NOT_A_RUG_PULL()
         // and then send 0x46 as the calldata so that it will get past the loop
-        bytes memory payload = abi.encodePacked(uint(0x6969696900000000000000000000000000000000000000000000000000000000), uint(0x0000004600000000000000000000000000000000000000000000000000000000));
-        // Note: When the two words above (0x69696969 right padded and 0x46 right padded offset by 4 bytes) are placed side by side
-        // then CALLDATA location 0x00 - 0x04 will contain exactly 0x69696969 and 0x04 - 0x24 contains a fully left padded 0x46
-        // In the EVM, "values" are always left padded.
+        bytes memory payload = abi.encodeWithSelector(bytes4(0x69696969), 0x46);
         vm.expectRevert(); // This attempt reverts!
         (bool success, bytes memory returnData) = address(vault).call{value: 0.001 ether}(payload);
         // After the "come and take it" WITHDRAW() line, there is no return or JUMP so it continues to execute
